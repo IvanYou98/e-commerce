5ba64bc754210301a6b6e727565f8cefe5277cb5
IvanYou98 <yuanfanyou@gmail.com>
2022-04-12 21:43:30 -0400

    implement navigation bar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
-import './categories.styles.scss'
-import CategoryItem from "./components/category-item/category-item.component";
+import Home from "./router/home/home.component";
+import {Routes, Route} from "react-router-dom";
+import Navigation from "./router/navigation/navigation.component";
+import {Fragment} from "react";
 
-const App = () => {
-
-    const categories = [
-        {
-            "id": 1,
-            "title": "hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-            "id": 2,
-            "title": "jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-            "id": 3,
-            "title": "sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-            "id": 4,
-            "title": "womens",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-            "id": 5,
-            "title": "mens",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-    ];
+const Shop = () => <Fragment>
+    This is the shop page.
+</Fragment>
 
 
-    return (
-        <div className='categories-container'>
-            {categories.map(category => {
-                return (
-                    <CategoryItem key={category.id} category = {category}/>
-                )
-            })}
-        </div>
-    );
-}
+const App = () => (
+    <Routes>
+        <Route path='/' element={<Navigation/>}>
+            <Route index element={<Home/>}/>
+            <Route path='shop' element={<Shop/>}/>
+        </Route>
+    </Routes>
+)
+
 
 export default App;
---
+++ src/components/category-list/category-list.component.js
@@ -0,0 +1,10 @@
+import CategoryItem from "../category-item/category-item.component";
+import './category-list.styles.scss'
+
+const CategoryList = ({categories}) => (
+    <div className='categories-container'>
+        {categories.map(category => <CategoryItem key={category.id} category={category}/>)}
+    </div>
+)
+
+export default CategoryList;
