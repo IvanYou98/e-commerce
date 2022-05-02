2efc927a6d9995955dde3b968d04811b71187df2
IvanYou98 <yuanfanyou@gmail.com>
2022-05-02 12:24:31 -0400

    migrate button to styled component

--- src/components/product-card/product-card.component.js
+++ src/components/product-card/product-card.component.js
@@ -1,12 +1,11 @@
 import './product-card.styles.scss'
-import {Button} from "../button/button.component";
+import {Button, BUTTON_TYPE_CLASSES} from "../button/button.component";
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
 
 export const ProductCard = ({product}) => {
     const {addItemToCart} = useContext(CartContext);
     const addProductToCart = () => {
-        // console.log(product, "is clicked!");
         addItemToCart(product)
     };
 
@@ -18,7 +17,7 @@ export const ProductCard = ({product}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-            <Button buttonType='inverted'
+            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}
                     onClick={addProductToCart}
             >Add to cart</Button>
         </div>
